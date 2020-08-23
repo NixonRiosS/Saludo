c1eded1393255b025e2008799941bfa3992e72f3
Nixon Rios Sanchez <nixonrs@hotmail.com>
2020-08-23 16:49:41 -0500

    Taller 1 ejercicio 1 programa saludador

---
+++ Taller1-EjercicioSaludador/Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace Taller1_EjercicioSaludador
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btn_presioname = new System.Windows.Forms.Button();
+            this.txt_nombre = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft YaHei", 24F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(165, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(432, 42);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "PROGRAMA SALUDADOR";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft YaHei", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(278, 120);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(198, 28);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Escriba su nombre";
+            // 
+            // btn_presioname
+            // 
+            this.btn_presioname.Font = new System.Drawing.Font("Microsoft YaHei", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_presioname.Location = new System.Drawing.Point(293, 248);
+            this.btn_presioname.Name = "btn_presioname";
+            this.btn_presioname.Size = new System.Drawing.Size(156, 69);
+            this.btn_presioname.TabIndex = 2;
+            this.btn_presioname.Text = "Presioname!";
+            this.btn_presioname.UseVisualStyleBackColor = true;
+            this.btn_presioname.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txt_nombre
+            // 
+            this.txt_nombre.Font = new System.Drawing.Font("Microsoft YaHei", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_nombre.Location = new System.Drawing.Point(251, 167);
+            this.txt_nombre.Name = "txt_nombre";
+            this.txt_nombre.Size = new System.Drawing.Size(253, 39);
+            this.txt_nombre.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.ClientSize = new System.Drawing.Size(736, 335);
+            this.Controls.Add(this.txt_nombre);
+            this.Controls.Add(this.btn_presioname);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btn_presioname;
+        private System.Windows.Forms.TextBox txt_nombre;
+    }
+}
+
---
+++ Taller1-EjercicioSaludador/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Taller1_EjercicioSaludador
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (txt_nombre.Text == "")
+            {
+                MessageBox.Show("El campo nombre debe estar diligenciado", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                MessageBox.Show("Hola " + txt_nombre.Text + " Espero de encuentres bien!", "Saludo a Usuario", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+         }
+    }
+}
